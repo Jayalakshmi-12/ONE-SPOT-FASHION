edfb21f86d7dff1bda0604b9cbe7a2f6bd7ecf8b
Jayalakshmi B S <jayalakshmibs35@gmail.com>
2026-04-26 10:57:49 +0530

    Refactor server.cjs

--- server.cjs
+++ server.cjs
@@ -5,17 +5,6 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const path = require('path');
-const { PythonShell } = require('python-shell');
-const fs = require('fs');
-require('dotenv').config();
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-// Middleware
-app.use(cors());
-app.use(express.json()); // Parse JSON requests
-app.use(bodyParser.urlencoded({ extended: true }));
 
 db.connect((err) => {
   if (err) {
